c57d6cbf55318f796f1d39674cb15bceec356952
veillette <veillettem@berea.edu>
2025-03-26 15:04:17 -0400

    Update Vite configuration to set base URL for GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  // So the build can be served from an arbitrary path
-  base: "./",
+  // Set the base URL to the repository name for GitHub Pages
+  base: process.env.GITHUB_PAGES ? '/dopplerEffectPhET/' : './',
 });
