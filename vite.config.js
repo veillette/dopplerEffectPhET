73a50a02748c87280eb19214386303b0e8c97e74
veillette <veillettem@berea.edu>
2025-03-27 16:20:39 -0400

    disable lint

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import { defineConfig } from "vite";
 // https://vitejs.dev/config/
 export default defineConfig({
   // Set the base URL to the repository name for GitHub Pages
+  // eslint-disable-next-line no-undef
   base: process.env.GITHUB_PAGES ? "/dopplerEffectPhET/" : "./",
 });
