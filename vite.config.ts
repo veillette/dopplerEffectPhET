7b341d0e25f3c30095f4d2fe057d6a7fe38a4e6a
veillette <veillettem@berea.edu>
2025-04-07 23:31:36 -0400

    - Remove vite-plugin-singlefile and terser from package.json and package-lock.json - Add new scripts for release and serve in package.json - Adjust build configuration in vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,22 +1,9 @@
 import { defineConfig } from "vite";
-import { viteSingleFile } from "vite-plugin-singlefile";
 
 export default defineConfig({
   // Set the base URL to the repository name for GitHub Pages
   base: process.env.GITHUB_PAGES ? "/dopplerEffectPhET/" : "./",
-  plugins: [viteSingleFile()],
   build: {
-    minify: "terser",
-    terserOptions: {
-      compress: {
-        drop_console: true,
-        drop_debugger: true,
-      },
-    },
-    rollupOptions: {
-      external: ["fsevents", "node:fs/promises", "node:perf_hooks"],
-    },
-    // Set a high asset limit to inline most assets
-    assetsInlineLimit: Infinity,
-  },
+    sourcemap: false,
+  }
 });
